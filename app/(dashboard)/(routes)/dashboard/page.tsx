ee7ff7581009eba097856a8211f48a0014494cce
nikhilhuirem <huiremnikhilsingh@gmail.com>
2024-01-19 18:28:35 +0530

    added conversation page

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -5,9 +5,11 @@ import { Card } from '@/components/ui/card';
 import { MessageSquare, ArrowRight} from 'lucide-react';
 import { cn } from '@/lib/utils';
 import { tools } from "@/constants";
+import { useRouter } from "next/navigation";
 
 
 export default function dashboardPAge() {
+  const router = useRouter();
   return <div className='mb-8 space-y-4'>
     <h2 className='text-2xl md:text-4xl font-bold text-center'>
       Explore the power of Ai
@@ -17,7 +19,7 @@ export default function dashboardPAge() {
     </p>
     <div className='px-4 md:px-20 lg:px-32 space-y-4'>
         {tools.map((tool) => (
-          <Card key={tool.href} className='p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer'>
+          <Card onClick={() => router.push(tool.href)} key={tool.href} className='p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer'>
             <div className='flex items-center gap-x-4'>
               <div className={cn("p-2 w-fit rounded-md", tool.color)}>
                 <tool.icon className={cn("w-8 h-8", tool.color)} />
